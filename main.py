f76e937323515fabb5ee32df1008c1b09ce2423a
Matheus Finger <matheusfinger@alu.ufc.br>
2023-05-17 14:16:11 -0300

    to entendendo até aqui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-
+import time
+import threading
 
 print("Criando a janela")
 janela = tk.Tk()
@@ -17,11 +18,22 @@ def clicou_botao():
     global texto_no_rotulo
     texto_no_rotulo.set("Você clicou no botão %i vezes" % i)
 
+def demora():
+    print("Começou a coisa demorada")
+    time.sleep(5)
+    print("Terminou a coisa demorada")
+
+def faz_demorado_paralelo():
+    new_thread = threading.Thread(target=demora)
+    new_thread.start()
+
 rotulo = ttk.Label(janela, textvariable=texto_no_rotulo)
 botao = ttk.Button(janela, text="Botão", command=clicou_botao)
+botaoDemora = ttk.Button(janela, text="Demora", command=faz_demorado_paralelo)
 
 rotulo.pack()
 botao.pack()
+botaoDemora.pack()
 
 # Iniciando o loop
 janela.mainloop()
